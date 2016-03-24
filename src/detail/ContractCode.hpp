493363b1d19703ffbabf50c954435a33fec5cf10
Victor Bucataru <ping@devktor.com>
2016-03-24 16:44:44 +0200

    bitprofile contract code

---
+++ src/detail/ContractCode.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+namespace BitProfile{
+
+struct ContractCode
+{
+
+    static const char * ProfileInterface;
+    static const char * ProfileFactoryInterface;
+    static const char * RegistrarFactoryInterface;
+    static const char * Registrar;
+    static const char * AddressAuth;
+    static const char * ProfileFactory;
+    static const char * RegistrarInterface;
+    static const char * Auth;
+    static const char * Owned;
+    static const char * MasterRegistrar;
+    static const char * RegistrarFactory;
+    static const char * RegistrarContext;
+    static const char * Profile;
+    static const char * RegistrarContextInterface;
+
+};
+
+
+}
