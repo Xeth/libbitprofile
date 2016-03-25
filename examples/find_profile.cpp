de3e9e36a3004f65e565bdfffe6f39454d1e0727
Victor Bucataru <ping@devktor.com>
2016-03-25 03:11:46 +0200

    find profile example

---
+++ examples/find_profile.cpp
@@ -0,0 +1,38 @@
+
+#include <iostream>
+
+#include "ethrpc/Provider.hpp"
+#include "bitprofile/Resolver.hpp"
+
+#include "utils/UnlockAccount.hpp"
+
+int main(int argc, char **argv)
+{
+    if(argc<2)
+    {
+        std::cout<<"usage : find_profile [uri]"<<std::endl;
+        return 1;
+    }
+
+    Ethereum::Connector::Provider provider;
+    provider.connect();
+
+    BitProfile::Resolver resolver(provider, BitProfile::MasterRegistrar::Test_Net);
+    BitProfile::Profile profile = resolver.lookup(argv[1]);
+
+    if(profile.isNull())
+    {
+        std::cout<<"profile not found"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"profile found : "<<profile.getAddress()<<std::endl;
+        std::cout<<"uri : "<<argv[1]<<std::endl;
+    }
+
+
+
+
+    return 0;
+}
+
