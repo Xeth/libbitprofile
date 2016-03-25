7e71f326f8002b9be399ecd8747de5405dfbefd3
Victor Bucataru <ping@devktor.com>
2016-03-25 18:34:27 +0200

    list registrars example

---
+++ examples/list_registrars.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+
+#include "ethrpc/Provider.hpp"
+#include "bitprofile/MasterRegistrar.hpp"
+
+
+using namespace BitProfile;
+
+
+int main(int argc, char **argv)
+{
+
+    Ethereum::Connector::Provider provider;
+    provider.connect();
+
+    MasterRegistrar registrar(provider, BitProfile::MasterRegistrar::Test_Net);
+    MasterRegistrar::URIList list = registrar.getURIList();
+
+    std::cout<<"registrars : "<<std::endl;
+    for(MasterRegistrar::URIList::Iterator it = list.begin(), end = list.end(); it!=end; ++it)
+    {
+        std::cout<<"* "<<it->toString()<<std::endl;
+    }
+
+    std::cout<<std::endl;
+
+    return 0;
+}
+
