6c324523698b64fbdd6a3ad42c205a8b332be848
Victor Bucataru <ping@devktor.com>
2016-03-30 23:07:28 +0300

    print registrar address

--- examples/list_registrars.cpp
+++ examples/list_registrars.cpp
@@ -14,13 +14,15 @@ int main(int argc, char **argv)
     Ethereum::Connector::Provider provider;
     provider.connect();
 
-    MasterRegistrar registrar(provider, BitProfile::Test_Net);
-    MasterRegistrar::URIList list = registrar.getURIList();
+    MasterRegistrar master(provider, BitProfile::Test_Net);
+    MasterRegistrar::URIList list = master.getURIList();
 
     std::cout<<"registrars : "<<std::endl;
     for(MasterRegistrar::URIList::Iterator it = list.begin(), end = list.end(); it!=end; ++it)
     {
-        std::cout<<"* "<<it->toString()<<std::endl;
+        std::cout<<"* "<<it->toString();
+        Registrar registrar = master.get(it->getIndex());
+        std::cout<<" : "<<registrar.getAddress()<<std::endl;
     }
 
     std::cout<<std::endl;
