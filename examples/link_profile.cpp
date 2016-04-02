efc5549389987e2649d35a97d9c54661310e9504
Victor Bucataru <ping@devktor.com>
2016-04-03 02:04:13 +0300

    link profile example

---
+++ examples/link_profile.cpp
@@ -0,0 +1,51 @@
+
+#include <iostream>
+
+#include "ethrpc/Provider.hpp"
+#include "bitprofile/Resolver.hpp"
+
+#include "utils/UnlockAccount.hpp"
+
+int main(int argc, char **argv)
+{
+    if(argc<4)
+    {
+        std::cout<<"usage : edit_profile [uri] [registrar] [name]"<<std::endl;
+        return 1;
+    }
+
+    Ethereum::Connector::Provider provider;
+    provider.connect();
+    UnlockAccount(provider);
+
+    BitProfile::Resolver resolver(provider, BitProfile::Test_Net);
+    BitProfile::Profile profile = resolver.lookupProfile(argv[1]);
+
+    if(profile.isNull())
+    {
+        std::cout<<"profile not found"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"profile found : "<<profile.getAddress()<<std::endl;
+        std::cout<<"uri : "<<argv[1]<<std::endl;
+        bool auth = profile.authenticate(profile.getSenderAddress(), BitProfile::Auth::Owner);
+        std::cout<<"auth : "<<auth<<std::endl<<std::flush;
+        BitProfile::Registrar registrar = resolver.lookupRegistrar(argv[2]);
+        if(registrar.isNull())
+        {
+            std::cout<<"registrar not found"<<std::endl;
+            return 1;
+        }
+
+        bool result = registrar.link(argv[3], profile.getAddress(), "");
+        std::cout<<"link : "<<result;
+
+    }
+
+
+
+
+    return 0;
+}
+
