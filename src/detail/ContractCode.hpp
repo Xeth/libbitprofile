da5f7641a00feef37ce582970818bf800b666416
Victor Bucataru <ping@devktor.com>
2016-04-03 17:04:33 +0300

    optional contract code

--- src/detail/ContractCode.hpp
+++ src/detail/ContractCode.hpp
@@ -7,11 +7,14 @@ namespace BitProfile{
 struct ContractCode
 {
 
+    static const std::string Profile;
+    static const std::string AddressAuth;
+
+#ifdef __INCLUDE_ALL_CONTRACTS__
     static const std::string ProfileInterface;
     static const std::string ProfileFactoryInterface;
     static const std::string RegistrarFactoryInterface;
     static const std::string Registrar;
-    static const std::string AddressAuth;
     static const std::string ProfileFactory;
     static const std::string RegistrarInterface;
     static const std::string Auth;
@@ -19,9 +22,8 @@ struct ContractCode
     static const std::string MasterRegistrar;
     static const std::string RegistrarFactory;
     static const std::string RegistrarContext;
-    static const std::string Profile;
     static const std::string RegistrarContextInterface;
-
+#endif
 };
 
 
