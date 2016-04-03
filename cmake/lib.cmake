7b96690b5f3c0e7ca3d717cfbf6fb83fec60fd46
Victor Bucataru <ping@devktor.com>
2016-04-03 21:33:33 +0300

    including boost

--- cmake/lib.cmake
+++ cmake/lib.cmake
@@ -1,13 +1,14 @@
 find_package(EthRPC REQUIRED)
 find_package(JsonCPP REQUIRED)
 find_package(CryptoPP REQUIRED)
+find_package(Boost REQUIRED)
 
 add_definitions(-D__ABI_ENABLED__)
 add_definitions(-DBOOST_PP_VARIADICS)
 
 include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/modules/CopyHeaders.cmake)
 
-include_directories(${JSONCPP_INCLUDE_DIR} ${CRYPTOPP_INCLUDE_DIR} ${ETHRPC_INCLUDE_DIR} ${CMAKE_CURRENT_SOURCE_DIR}/src)
+include_directories(${Boost_INCLUDE_DIRS} ${JSONCPP_INCLUDE_DIR} ${CRYPTOPP_INCLUDE_DIR} ${ETHRPC_INCLUDE_DIR} ${CMAKE_CURRENT_SOURCE_DIR}/src)
 file(GLOB_RECURSE SOURCES "src/*.cpp")
 
 
