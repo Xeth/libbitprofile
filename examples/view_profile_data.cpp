70c4f45452c5c2c68112a38a30cff09862cade4f
Victor Bucataru <ping@devktor.com>
2016-04-12 00:09:47 +0300

    view profile data example

---
+++ examples/view_profile_data.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+
+#include "ethrpc/Provider.hpp"
+#include "bitprofile/Resolver.hpp"
+
+#include "utils/UnlockAccount.hpp"
+
+int main(int argc, char **argv)
+{
+    if(argc<3)
+    {
+        std::cout<<"usage : view_profile_data [uri] [key]"<<std::endl;
+        return 1;
+    }
+
+    Ethereum::Connector::Provider provider;
+    provider.connect();
+
+    BitProfile::Resolver resolver(provider, BitProfile::Test_Net);
+    BitProfile::Profile profile = resolver.lookupProfile(argv[1]);
+
+    if(profile.isNull())
+    {
+        std::cout<<"profile not found"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"profile found : "<<profile.getAddress()<<std::endl;
+        std::cout<<"uri : "<<argv[1]<<std::endl;
+        std::cout<<"data["<<argv[2]<<"]="<<profile.get(argv[2])<<std::endl;
+    }
+
+
+
+
+    return 0;
+}
+
